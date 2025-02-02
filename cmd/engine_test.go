fdfc68b510faba50ce6b495699e6233b5d8d833e
Alex Kraasch <alex@kraasch.eu>
2025-02-02 03:52:07 +0100

    Courtesy flush.

--- cmd/engine_test.go
+++ cmd/engine_test.go
@@ -40,28 +40,34 @@ var suites = []TestSuite{
   * Test for the function DateAsHeader().
   */
   {
-    DateAsHeader,
+    func(in string) (out string) {
+      out = DateAsHeader(in)
+      return
+    },
     []TestList{
-      {"Date 1 digit day, leading space",    "2025-02-01", " 1. February, 2025"},
-      {"Date 2 digit day, no leading space", "2025-12-12", "12. December, 2025"},
+      {"date-1-digit-day+leading-space",    "2025-02-01", " 1. February, 2025"},
+      {"date-2-digit-day+no-leading-space", "2025-12-12", "12. December, 2025"},
     },
   },
   /*
   * Test for the function MonthAsCalendar().
   */
   {
-    MonthAsCalendar,
+    func(in string) (out string) {
+      out = MonthAsCalendar(in)
+      return
+    },
     []TestList{
       {
-        testName:       "February with 28 days",
+        testName:       "february-with-28-days",
         inputString:    "2025-02",
         expectedValue:
         "Mo Tu We Th Fr Sa Su" + NL +
-        "       1  2  3  4  5" + NL +
-        " 6  7  8  9 10 11 12" + NL +
-        "13 14 15 16 17 18 19" + NL +
-        "20 21 22 23 24 25 26" + NL +
-        "27 28               " + NL,
+        "                1  2" + NL +
+        " 3  4  5  6  7  8  9" + NL +
+        "10 11 12 13 14 15 16" + NL +
+        "17 18 19 20 21 22 23" + NL +
+        "24 25 26 27 28      " + NL,
       },
     },
   },
