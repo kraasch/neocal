c7e8f92408cecf9f61fce4f3bbce5a8378718e52
Alex Kraasch <alex@kraasch.eu>
2025-02-02 02:36:18 +0100

    Courtesy flush.

---
+++ cmd/engine_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+
+  // this is a test.
+  "testing"
+
+  // prints.
+  "fmt"
+
+  // other imports.
+)
+
+var (
+  NL = fmt.Sprintln()
+)
+
+var suites = []TestSuite{
+  {
+    DateAsHeader,
+    []TestList{
+      {"Date 1 digit day, leading space",    "2025-02-01", " 1. February, 2025"},
+      {"Date 2 digit day, no leading space", "2025-12-12", "12. December, 2025"},
+    },
+  },
+}
+
+
+type TestList struct {
+  testName          string
+  inputString       string
+  expectedString    string
+}
+
+type TestSuite struct {
+  functionUnderTest func(string) string
+  tests             []TestList
+}
+
+func TestAll(t *testing.T) {
+  for _, suite := range suites {
+    for _, test := range suite.tests {
+      name := test.testName
+      exp := test.expectedString
+      got := test.functionUnderTest(test.inputString)
+      if exp != got {
+        t.Errorf("In '%s': Exptected '%#v', got actually '%#v'.\n", name, exp, got)
+      }
+    }
+  }
+}
+
+// import ("time")
+// parsedDate, err := time.Parse(layout, dateStr)
+//     if err != nil {
+//         fmt.Println("Error parsing date:", err)
+//         return
+//     }
+
+//   actualArrays := DateAsArrays()
+//   expectArrays := [][]string {
+//     { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su" },
+//     { "  ", "  ", " 1", " 2", " 3", " 4", " 5" },
+//     { " 6", " 7", " 8", " 9", "10", "11", "12" },
+//     { "13", "14", "15", "16", "17", "18", "19" },
+//     { "20", "21", "22", "23", "24", "25", "26" },
+//     { "27", "28", "  ", "  ", "  ", "  ", "  " },
+//   }
+//   actualBlob = DateAsBlob()
+//   expectedBlob = "Mo Tu We Th Fr Sa Su" + NL +
+//            "       1  2  3  4  5" + NL +
+//            " 6  7  8  9 10 11 12" + NL +
+//            "13 14 15 16 17 18 19" + NL +
+//            "20 21 22 23 24 25 26" + NL +
+//            "27 28               " + NL
+// }
+
