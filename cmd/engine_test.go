6909291ad16ce6a9668f039e02b13b20a15ee5c3
Alex Kraasch <alex@kraasch.eu>
2025-02-02 03:10:09 +0100

    Courtesy flush.

--- cmd/engine_test.go
+++ cmd/engine_test.go
@@ -28,7 +28,15 @@ type TestSuite struct {
 
 var suites = []TestSuite{
   /*
-  * Test DateAsHeader.
+  * Test for the function TEMPLATE().
+  */
+  {
+    nil,
+    []TestList{
+    },
+  },
+  /*
+  * Test for the function DateAsHeader().
   */
   {
     DateAsHeader,
@@ -38,15 +46,24 @@ var suites = []TestSuite{
     },
   },
   /*
-  * Test DateAsHeader.
+  * Test for the function MonthAsCalendar().
   */
-  //{
-  //  DateAsHeader,
-  //  []TestList{
-  //    {"Date 1 digit day, leading space",    "2025-02-01", " 1. February, 2025"},
-  //    {"Date 2 digit day, no leading space", "2025-12-12", "12. December, 2025"},
-  //  },
-  //},
+  {
+    MonthAsCalendar,
+    []TestList{
+      {
+        testName:       "February with 28 days",
+        inputString:    "2025-02",
+        expectedString:
+        "Mo Tu We Th Fr Sa Su" + NL +
+        "       1  2  3  4  5" + NL +
+        " 6  7  8  9 10 11 12" + NL +
+        "13 14 15 16 17 18 19" + NL +
+        "20 21 22 23 24 25 26" + NL +
+        "27 28               " + NL,
+      },
+    },
+  },
 }
 
 func TestAll(t *testing.T) {
@@ -62,7 +79,7 @@ func TestAll(t *testing.T) {
   }
 }
 
-//   actualArrays := DateAsArrays()
+//   actualArrays := MonthAsArrays
 //   expectArrays := [][]string {
 //     { "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su" },
 //     { "  ", "  ", " 1", " 2", " 3", " 4", " 5" },
@@ -71,12 +88,4 @@ func TestAll(t *testing.T) {
 //     { "20", "21", "22", "23", "24", "25", "26" },
 //     { "27", "28", "  ", "  ", "  ", "  ", "  " },
 //   }
-//   actualBlob = DateAsBlob()
-//   expectedBlob = "Mo Tu We Th Fr Sa Su" + NL +
-//            "       1  2  3  4  5" + NL +
-//            " 6  7  8  9 10 11 12" + NL +
-//            "13 14 15 16 17 18 19" + NL +
-//            "20 21 22 23 24 25 26" + NL +
-//            "27 28               " + NL
-// }
 
