ea739c8e965f13906c23a65d5ea781bad1c41622
Alex Kraasch <alex@kraasch.eu>
2025-02-04 02:34:34 +0100

    Test controller.

--- pkg/calcontroller/controller.go
+++ pkg/calcontroller/controller.go
@@ -2,25 +2,61 @@
 package calcontroller
 
 import (
-  "fmt"
-  "strconv"
+  "time"
 )
 
 type Controller struct {
-  state int
+  cal time.Time
 }
 
-func (c *Controller) Control(in string) (ok bool) {
-  val, err := strconv.Atoi(in)
+func (c *Controller) SetDate(in string) (ok bool) {
+  t, err := time.Parse(time.DateOnly, in)
+  c.cal = t
   if err == nil {
     ok = true
   }
-  c.state = val
   return
 }
 
-func (c *Controller) ReadState() (out string) {
-  out = fmt.Sprintf("%d", c.state)
+func (c *Controller) Control(action string, unit string) (ok bool) {
+  ok = true // default to true.
+
+  // evaluate action.
+  dir := 0
+  if action == "next" {
+    dir = 1
+  } else if action == "prev" {
+    dir = -1
+  } else {
+    ok = false
+    return
+  }
+
+  // evaluate unit.
+  days   := 0
+  months := 0
+  years  := 0
+  if unit == "day" {
+    days = 1
+  } else if unit == "week" {
+    days = 7
+  } else if unit == "month" {
+    months = 1
+  } else if unit == "year" {
+    years = 1
+  } else {
+    ok = false
+    return
+  }
+
+  // update date according to aciton.
+  c.cal = c.cal.AddDate(years * dir, months * dir, days * dir)
+
+  return
+}
+
+func (c *Controller) ReadDate() (out string) {
+  out = c.cal.Format(time.DateOnly)
   return
 }
 
--- pkg/calcontroller/controller_test.go
+++ pkg/calcontroller/controller_test.go
@@ -2,24 +2,25 @@
 package calcontroller
 
 import (
+  // test.
   "testing"
-  "fmt"
   "github.com/kraasch/godiff/godiff"
-)
 
-var (
-  NL = fmt.Sprintln()
+  // other.
+  //"time"
+  //"fmt"
 )
 
 type TestList struct {
   testName          string
   isMulti           bool
+  initialValue      string
   inputArr          []string
   expectedValue     string
 }
 
 type TestSuite struct {
-  functionUnderTest func(...string) string
+  functionUnderTest func(string, ...string) string
   tests             []TestList
 }
 
@@ -29,23 +30,183 @@ var suites = []TestSuite{
   */
   {
     functionUnderTest: 
-    func(in ...string) (out string) {
-      inputData := in[0]
-      var c Controller
-      ok := c.Control(inputData)
+    func(setDate string, in ...string) string {
+      c := Controller{}
+      ok := c.SetDate(setDate)
       if ok {
-        out = "Wrote value: "
+        action := ""
+        for i, val := range in {
+          unit := val // 2nd loop: treat val as unit.
+          if i % 2 == 1 { // only trigger every 2nd loop.
+            c.Control(action, unit)
+          }
+          action = val // 2nd loop + 1: treat val as new action for next loop.
+        }
       }
-      out += c.ReadState()
-      return
+      return c.ReadDate()
     },
-    tests: 
+    tests:
     []TestList{
       {
-        testName:      "controller_create_00",
+        testName:      "controller_next_00",
         isMulti:       false,
-        inputArr:      []string{"42"},
-        expectedValue: "Wrote value: 42",
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "2025-01-05",
+      },
+      {
+        testName:      "controller_next_01",
+        isMulti:       false,
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "next", "day",
+          "next", "day",
+        },
+        expectedValue: "2025-01-06",
+      },
+      {
+        testName:      "controller_next_02",
+        isMulti:       false,
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "next", "day",
+          "next", "week",
+        },
+        expectedValue: "2025-01-12",
+      },
+      {
+        testName:      "controller_next_03",
+        isMulti:       false,
+        initialValue:  "2000-01-01",
+        inputArr:      []string{
+          "next", "year",
+          "next", "month",
+          "next", "day",
+        },
+        expectedValue: "2001-02-02",
+      },
+      {
+        testName:      "controller_prev_00",
+        isMulti:       false,
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "prev", "day",
+        },
+        expectedValue: "2025-01-03",
+      },
+      {
+        testName:      "controller_prev_01",
+        isMulti:       false,
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "prev", "day",
+          "prev", "day",
+        },
+        expectedValue: "2025-01-02",
+      },
+      {
+        testName:      "controller_prev_02",
+        isMulti:       false,
+        initialValue:  "2025-01-04",
+        inputArr:      []string{
+          "prev", "day",
+          "prev", "week",
+        },
+        expectedValue: "2024-12-27",
+      },
+      {
+        testName:      "controller_prev_03",
+        isMulti:       false,
+        initialValue:  "2000-01-01",
+        inputArr:      []string{
+          "prev", "year",
+          "prev", "month",
+          "prev", "day",
+        },
+        expectedValue: "1998-11-30",
+      },
+      {
+        testName:      "controller_04",
+        isMulti:       false,
+        initialValue:  "2000-01-01",
+        inputArr:      []string{
+          "next", "year",
+          "next", "year",
+          "prev", "month",
+          "next", "week",
+          "next", "month",
+          "prev", "day", // all, but this one cancel each other.
+          "prev", "year",
+          "prev", "week",
+          "prev", "year",
+        },
+        expectedValue: "1999-12-31",
+      },
+      {
+        testName:      "leap-day_no_100-years_00",
+        isMulti:       false,
+        initialValue:  "1900-02-28",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "1900-03-01",
+      },
+      {
+        testName:      "leap-day_no_100-years_01",
+        isMulti:       false,
+        initialValue:  "2100-02-28",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "2100-03-01",
+      },
+      {
+        testName:      "leap-day_yes_all-4-years_00",
+        isMulti:       false,
+        initialValue:  "1904-02-28",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "1904-02-29",
+      },
+      {
+        testName:      "leap-day_yes_all-4-years_01",
+        isMulti:       false,
+        initialValue:  "1996-02-28",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "1996-02-29",
+      },
+      {
+        testName:      "leap-day_yes_all-400-years_00",
+        initialValue:  "2000-02-28",
+        inputArr:      []string{
+          "next", "day",
+        },
+        expectedValue: "2000-02-29",
+      },
+      {
+        testName:      "controller_order-matters_00",
+        isMulti:       false,
+        initialValue:  "2000-01-01",
+        inputArr:      []string{
+          "prev", "day",
+          "prev", "month",
+        },
+        expectedValue: "1999-12-01",
+      },
+      {
+        testName:      "controller_order-matters_01",
+        isMulti:       false,
+        initialValue:  "2000-01-01",
+        inputArr:      []string{
+          "prev", "month",
+          "prev", "day",
+        },
+        expectedValue: "1999-11-30",
       },
     },
   },
@@ -57,7 +218,7 @@ func TestAll(t *testing.T) {
       name := test.testName
       t.Run(name, func(t *testing.T) {
         exp := test.expectedValue
-        got := suite.functionUnderTest(test.inputArr...)
+        got := suite.functionUnderTest(test.initialValue, test.inputArr...)
         if exp != got {
           if test.isMulti {
             t.Errorf("In '%s':\n", name)
